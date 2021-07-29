e58509f03b74f98663c8e50dd481a4767fd62a22
Konstantinos Paterakis <kpaterakis@marpoint.gr>
2021-07-29 21:02:13 +0300

    Compatibility workaround for Angular.

--- lib/ngx-leaflet-fullscreen.component.d.ts
+++ lib/ngx-leaflet-fullscreen.component.d.ts
@@ -1,9 +1,7 @@
 import { OnInit, OnDestroy } from '@angular/core';
-import { Map, Control, FullscreenOptions } from 'leaflet';
+import { Map} from 'leaflet';
 import '../../../../node_modules/@runette/leaflet-fullscreen/dist/Leaflet.fullscreen.js';
 export declare class FullscreenControlComponent implements OnInit, OnDestroy {
-    private _map;
-    control: Control.Fullscreen;
     constructor();
     ngOnInit(): void;
     ngOnDestroy(): void;
